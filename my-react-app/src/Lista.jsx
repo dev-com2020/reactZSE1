230da8826c4bbd549cb643f69e3d21c50f56c9fd
tomaszkaniecki <kwp@tlen.pl>
2024-05-13 13:53:28 +0200

    13:35/1

--- my-react-app/src/Lista.jsx
+++ my-react-app/src/Lista.jsx
@@ -1,15 +1,31 @@
 // import * as ReactDOM from "react-dom"
 
 function Lista() {
-    return (
+    const array = ["Jeden", "Dwa", "Trzy"]
+    const object = {
+        first: 1,
+        second: 2,
+        third: 3
+    }
 
-        
-            <>
-                <p>
-                    Jestem z listy 👏
-                </p>
-            </>
-        
+    return (
+       <>
+            <ul>
+                <h1>Array</h1>
+                {array.map((i) => (
+                    <li key={i}>{i}</li>
+                ))}
+            </ul>
+            <h2>Object</h2>
+            <ul>
+                {Object.keys(object).map((i) => (
+                    <li key={i}>
+                        <strong>{i}: </strong>
+                        {object[i]}
+                    </li>
+                ))}
+            </ul>
+        </>
     )
 }
 export default Lista
