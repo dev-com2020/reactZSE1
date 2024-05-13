fbaf94037a90c4967f587d5b7592d78f6bcd66aa
tomaszkaniecki <kwp@tlen.pl>
2024-05-13 11:58:16 +0200

    first commit

---
+++ my-react-app/src/Hello.jsx
@@ -0,0 +1,10 @@
+import * as ReactDOM from "react-dom"
+
+const hello = ReactDOM.createRoot(document.getElementById("hello"))
+hello.render(
+    <>
+    <p>
+        Witaj, <strong>React z JSX</strong>
+    </p>
+    </>
+)
