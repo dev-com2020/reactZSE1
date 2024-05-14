de1a265ec258a4fe6059c20e635bbe3f73b9600f
tomaszkaniecki <kwp@tlen.pl>
2024-05-14 11:29:45 +0200

    setcolor

--- my-react-app/src/MyInput.jsx
+++ my-react-app/src/MyInput.jsx
@@ -1,17 +1,20 @@
 import React from "react"
+import './MyInput.css'
 
 function MyInput() {
     const [changeMessage, setChangeMessage] = React.useState('')
     const [blurMessage, setBlurMessage] = React.useState('')
+    const [inputClass, setInputClass] = React.useState('')
 
     const onChange = () => {
         setChangeMessage('zmiana!')
     }
     const onBlur = () => {
         setBlurMessage('opuszczam pole!')
+        setInputClass('red-color')
     }
     return (<>
-    <input onChange={onChange} onBlur={onBlur} />
+    <input onChange={onChange} onBlur={onBlur} className={inputClass}/>
     <p>{changeMessage}</p>
     <p>{blurMessage}</p>
     </>) 
